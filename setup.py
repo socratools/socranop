abe7339343221d4d1179da1cf4335d4ac347cb08
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-06-06 22:34:31 +0200

    setup: Add classifiers for Python versions 3.6 to 3.10

--- setup.py
+++ setup.py
@@ -34,8 +34,14 @@ setup(
     url="https://github.com/lack/soundcraft-utils",
     license="MIT",
     packages=find_packages(),
+    # https://pypi.org/classifiers/
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Multimedia :: Sound/Audio :: Mixers",
