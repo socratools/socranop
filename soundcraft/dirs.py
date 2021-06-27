92e1d6af4e731882a9ec4cf999b94acc06036787
Hans Ulrich Niedermann <hun@n-dimensional.de>
2021-06-27 10:26:22 +0200

    Indent code preparing for next commit
    This only indents the code one step so that the `git diff` for the
    next commit can be smaller and show the changes better.

--- soundcraft/dirs.py
+++ soundcraft/dirs.py
@@ -30,59 +30,63 @@ import soundcraft.constants as const
 
 
 def serviceExePath():
-    return exePath().parent / const.BASE_EXE_SERVICE
+    if True:
+        return exePath().parent / const.BASE_EXE_SERVICE
 
 
 def exePath():
-    exename = Path(sys.argv[0]).resolve()
-    if exename.suffix == ".py":
-        raise ValueError("Running out of a module-based execution is not supported")
-    return exename
+    if True:
+        exename = Path(sys.argv[0]).resolve()
+        if exename.suffix == ".py":
+            raise ValueError("Running out of a module-based execution is not supported")
+        return exename
 
 
 def find_datadir():
-    exe_path = exePath()
-    # print("exe_path", exe_path)
-    for prefix in [Path("/usr/local"), Path("/usr"), Path("~/.local").expanduser()]:
-        for sx_dir in ["bin", "sbin", "libexec"]:
-            for sx in [
-                const.BASE_EXE_CLI,
-                const.BASE_EXE_GUI,
-                const.BASE_EXE_SERVICE,
-                const.BASE_EXE_INSTALLTOOL,
-            ]:
-                sx_path = prefix / sx_dir / sx
-                # print("sx_path", sx_path)
-                if sx_path == exe_path:
-                    return prefix / "share"
-                try:
-                    exe_path.relative_to(prefix)  # ignore result
+    if True:
+        exe_path = exePath()
+        # print("exe_path", exe_path)
+        for prefix in [Path("/usr/local"), Path("/usr"), Path("~/.local").expanduser()]:
+            for sx_dir in ["bin", "sbin", "libexec"]:
+                for sx in [
+                    const.BASE_EXE_CLI,
+                    const.BASE_EXE_GUI,
+                    const.BASE_EXE_SERVICE,
+                    const.BASE_EXE_INSTALLTOOL,
+                ]:
+                    sx_path = prefix / sx_dir / sx
+                    # print("sx_path", sx_path)
+                    if sx_path == exe_path:
+                        return prefix / "share"
+                    try:
+                        exe_path.relative_to(prefix)  # ignore result
 
-                    # If this is
-                    # ``/home/user/.local/share/virtualenvs/soundcraft-utils-ABCDEFG/bin/soundcraft_installtool``,
-                    # then the D-Bus and XDG config can either go into
-                    # ``/home/user/.local/share/virtualenvs/soundcraft-utils-ABCDEFG/share/``
-                    # and be ignored, or go into
-                    # ``/home/user/.local/share/`` and work. We choose
-                    # the latter.
-                    return prefix / "share"
-                except ValueError:
-                    pass  # exe_path is not a subdir of prefix
-    raise ValueError(f"Exe path is not supported: {exe_path!r}")
+                        # If this is
+                        # ``/home/user/.local/share/virtualenvs/soundcraft-utils-ABCDEFG/bin/soundcraft_installtool``,
+                        # then the D-Bus and XDG config can either go into
+                        # ``/home/user/.local/share/virtualenvs/soundcraft-utils-ABCDEFG/share/``
+                        # and be ignored, or go into
+                        # ``/home/user/.local/share/`` and work. We choose
+                        # the latter.
+                        return prefix / "share"
+                    except ValueError:
+                        pass  # exe_path is not a subdir of prefix
+        raise ValueError(f"Exe path is not supported: {exe_path!r}")
 
 
 def find_statedir():
-    """The statedir where the device state files are stored
+    if True:
+        """The statedir where the device state files are stored
 
-    The state directory for a user session service must be somewhere
-    in the user's ``$HOME``, and a config file is a good place.
-    """
-    config_dir = Path("~/.config").expanduser()
+            The state directory for a user session service must be somewhere
+            in the user's ``$HOME``, and a config file is a good place.
+            """
+        config_dir = Path("~/.config").expanduser()
 
-    xdg_config_home = getenv("XDG_CONFIG_HOME")
-    if xdg_config_home:  # neither None nor empty string
-        xdg_config_home_path = Path(xdg_config_home)
-        config_dir = xdg_config_home_path
+        xdg_config_home = getenv("XDG_CONFIG_HOME")
+        if xdg_config_home:  # neither None nor empty string
+            xdg_config_home_path = Path(xdg_config_home)
+            config_dir = xdg_config_home_path
 
-    statedir = config_dir / const.PACKAGE / "state"
-    return statedir
+        statedir = config_dir / const.PACKAGE / "state"
+        return statedir
