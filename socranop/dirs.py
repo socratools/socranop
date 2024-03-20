socratools/socranop

commit 83a74171f13bf703afdf4631850429021892304e

it: Add pbi --force-prefix for NixOS installation
When invoked with `--force-prefix`, `socranop-installtool
package-build-install` will accept ANY install location as
a proper installation prefix.

This is useful for e.g. for NixOS with install locations like
`/nix/store/w3y50ifd6zss5j09n21c36wk45ip84x0-socranop-0.4.92`.

The `--chroot CHROOT` and `--force-prefix` arguments mutually
exclude each other at this time, but we might change that later.

Closes: https://github.com/socratools/socranop/issues/77

socranop/dirs.py
@@ -90,6 +90,9 @@ class AbstractDirs(metaclass=abc.ABCMeta):
         ps = ", ".join([("%s=%r" % (p, c(getattr(self, p)))) for p in props])
         return f"{self.__class__.__name__}({ps})"
 
+    def __repr__(self):
+        return self.__str__()
+
     @property
     def chroot(self):
         return self._chroot
@@ -101,6 +104,7 @@ class AbstractDirs(metaclass=abc.ABCMeta):
             rel_path = path.relative_to(self.chroot)
             return Path(f"/{rel_path}")
 
+    @classmethod
     @property
     def exePath(self):
         """The path the currently running executable"""
@@ -197,6 +201,29 @@ class GlobalDirs(AbstractDirs):
     pass  # class GlobalDirs
 
 
+class AnyPrefixDirs(GlobalDirs):
+    """Global install to ANY prefix.
+
+    Should be useful for NixOS installs.
+    """
+
+    def __init__(self, chroot=None):
+        exe_path = GlobalDirs.exePath
+        if chroot is not None:
+            exe_path_relative = exe_path.relative_to(Path(chroot))
+            rooted_path = Path("/") / exe_path_relative
+        else:
+            rooted_path = exe_path
+        assert rooted_path.parent.name == "bin"
+        rooted_prefix = rooted_path.parent.parent
+        self.PREFIX = rooted_prefix.resolve()
+        super().__init__(chroot=chroot)
+
+    @property
+    def udev_rulesdir(self):
+        return self.PREFIX / "lib/udev/rules.d"
+
+
 class UsrDirs(GlobalDirs):
     """Global install to /usr"""
 
@@ -231,10 +258,15 @@ class HomeDirs(AbstractDirs):
 __dir_instance = None
 
 
-def init_dirs(chroot=None):
+def init_dirs(chroot=None, force_prefix=False):
     global __dir_instance
 
     assert __dir_instance is None
+
+    if force_prefix is not None:
+        __dir_instance = AnyPrefixDirs(chroot)
+        debug("Using dirs:", __dir_instance)
+        return __dir_instance
 
     for cls in [UsrLocalDirs, UsrDirs, HomeDirs]:
         try:

socranop/data/bash-completion/socranop-installtool
@@ -17,7 +17,7 @@ _socranop_installtool()
 	    package-build-install)
 		subcommand="$word"
 		REMAINING_OPTIONS=()
-		for word in --chroot -h --help
+		for word in --chroot --force-prefix -h --help
 		do
 		    REMAINING_OPTIONS["$word"]=t
 		done
@@ -42,7 +42,7 @@ _socranop_installtool()
 		    COMPREPLY=($(compgen -A directory -- "$2"))
 		    return 0
 		    ;;
-		-h | --help)
+		-h | --help | --force-prefix)
                     COMPREPLY=()
                     return 0
                     ;;
@@ -51,7 +51,7 @@ _socranop_installtool()
 	    do
 		word="${COMP_WORDS[$i]}"
 		case "$word" in
-		    -h | --help)
+		    -h | --help | --force-prefix)
 			COMPREPLY=()
 			return 0
 			;;

socranop/data/man/socranop-installtool.8
@@ -56,11 +56,12 @@ socranop\-installtool - Hook socranop into the system post-install (or do the re
 .\"                         write the script of sudo commands to the given
 .\"                         FILENAME
 .\"
-.\" usage: socranop-installtool package-build-install [-h] --chroot CHROOT
+.\" usage: socranop-installtool package-build-install [-h] ( --chroot CHROOT | --force-prefix )
 .\"
 .\" options:
 .\"   -h, --help       show this help message and exit
 .\"   --chroot CHROOT  package build root chroot directory
+.\"   --force-prefix   force acceptance of non-standard installation prefix
 .\"
 .\"
 .\" ======================================================================
@@ -78,8 +79,9 @@ socranop\-installtool - Hook socranop into the system post-install (or do the re
 .\" .B socranop\-installtool
 .\" .RB [\| \-v \|]
 .\" .RB [\| \-n \|]
-.\" .B package\-build\-install
+.\" .BR package\-build\-install (
 .\" .BI \-\-chroot\  CHROOT
+.\" .RI | \-\-force\-prefix )
 .\" .br
 .\" .B socranop\-installtool
 .\" .RB [\| \-v \|]
@@ -155,9 +157,11 @@ If \fI\-\-sudo\-script\fR is not given, prints the sudo script to stdout as info
 .SS \fIsocranop\-installtool package\-build\-install\fR command
 Call \fIsocranop\-installtool package\-build\-install\fR from your package build script when you are \fIbuilding\fR an OS binary package like \fI*.rpm\fR, \fI*.deb\fR, AUR \fI*.tar.zstd\fR, etc.
 
-This installs the configuration and resource files into the \fICHROOT\fR, prints some commands to \fIstdout\fR so that the packager can decide whether and where to write package hook scripts running those commands (e.g. \fIudevadm trigger\fR for ONLY the Notepad devices), and finally removes \fIsocranop\-installtool\fR related files  from the \fICHROOT\fR.
+This installs the configuration and resource files into the proper location (\fIprefix\fR or \fICHROOT\fR), prints some commands to \fIstdout\fR so that the packager can decide whether and where to write package hook scripts running those commands (e.g. \fIudevadm trigger\fR for ONLY the Notepad devices), and finally removes \fIsocranop\-installtool\fR related files  from the installation location (\fIprefix\fR or \fICHROOT\fR).
 
-As uninstalling an OS package is handled by the package manager (\fIdnf\fR/\fIrpm\fR, \fIapt\fR/\fIdpkg\fR, \fIpacman\fR, etc.), \fIsocranop\-installtool\fR only needs to care about installing, not about uninstalling. However, the packager may want to put some of the command suggestions printed by \fIsocranop\-installtool package\-build\-install\fR into some uninstall hook scripts.
+As uninstalling an OS package is handled by the package manager (\fIdnf\fR/\fIrpm\fR, \fIapt\fR/\fIdpkg\fR, \fIpacman\fR, etc.), \fIsocranop\-installtool\fR only needs to care about installing, not about uninstalling.
+
+However, the packager may want to put some of the command suggestions printed by \fIsocranop\-installtool package\-build\-install\fR into some install/uninstall/update hook scripts.
 .\"
 .\"
 .\" ======================================================================
@@ -200,9 +204,12 @@ to execute those privileged operations after the \fIpip uninstall socranop\fR ha
 .\"
 .\"
 .SS Options for \fIpackage\-build\-install\fR command
+One of the following arguments is required:
 .TP
 .BI \-\-chroot\  CHROOT
-Give the package's build chroot \fICHROOT\fR to install to (required argument).
+Give the package's build chroot \fICHROOT\fR to install to.
+.B \-\-force\-prefix
+Force \fIsocranop\-installtool package-build-install\fR into accepting \fIsocranop\-installtool\fR's \fIprefix\fR whatever it may be. This should work for the weird NixOS installation locations like \fI/nix/store/w3y50ifd6zss5j09n21c36wk45ip84x0-socranop-0.4.92\fR.
 .\"
 .\" ======================================================================
 .\"

socranop/installtool.py
@@ -1205,25 +1205,32 @@ def parse_argv(argv=None):
     add_sudo_script_argument(parser_ppi)
     add_sudo_script_argument(parser_ppu)
 
-    parser_pbi.add_argument(
+    mutex_pbi = parser_pbi.add_mutually_exclusive_group(required=True)
+    mutex_pbi.add_argument(
         "--chroot",
         metavar="CHROOT",
         help="package build root chroot directory",
         type=Path,
         default=None,
-        required=True,
+    )
+    mutex_pbi.add_argument(
+        "--force-prefix",
+        help="force acceptance of non-standard installation prefix",
+        action="store_true",
     )
 
     args = parser.parse_args(argv)
     print("args after parse_args:", args)
 
-    if hasattr(args, "chroot"):
+    if hasattr(args, "chroot") and hasattr(args, "force_prefix"):
         # Initialize the dirs object with the chroot given so that later
         # calls to get_dirs() will yield an object which uses the same
         # chroot value.
-        dirs = init_dirs(chroot=args.chroot)
-    else:
+        dirs = init_dirs(chroot=args.chroot, force_prefix=args.force_prefix)
+    elif (not hasattr(args, "chroot")) and (not hasattr(args, "force_prefix")):
         dirs = init_dirs()
+    else:
+        parser.error("Internal error related to chroot and force_prefix")
     common.debug("Using dirs", dirs)
 
     print("args", args)

commit c7193c661c51c97a04a5922e3fdd0b8fd6e02ba8

dirs: Fix force_prefix handling
Fixes bug introduced in commit 83a74171f13bf703afdf4631850429021892304e:

Installations to virtualenvs in ~/.local/ resulted in files intended
for well-known directories in ~/.local/ to end up somewhere inside
a virtualenv directory where nobody is looking for them.

socranop/dirs.py
@@ -262,8 +262,9 @@ def init_dirs(chroot=None, force_prefix=False):
     global __dir_instance
 
     assert __dir_instance is None
+    assert force_prefix is not None
 
-    if force_prefix is not None:
+    if force_prefix:
         __dir_instance = AnyPrefixDirs(chroot)
         debug("Using dirs:", __dir_instance)
         return __dir_instance